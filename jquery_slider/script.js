de0f35ce6c24bf8d52e77bebe6f1de6e95c7b948
totokiasuka <167288774+totokiasuka@users.noreply.github.com>
2024-06-05 15:54:05 +0900

    Add files via upload

---
+++ jquery_slider/script.js
@@ -0,0 +1,14 @@
+// JavaScript
+//console.log('Hello world!');
+$(function(){
+    $('.bxslider').bxSlider({
+      // オプションを書く場所
+      mode: 'fade',
+      captions: true,
+      slideWidth: 600,
+      auto: true,
+      spead: 500,
+      pause: 4000,
+      autoHover: true,
+    });
+  });
